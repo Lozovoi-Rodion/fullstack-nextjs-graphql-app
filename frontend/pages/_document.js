6f9c4b5d06df920b92ea17f48394eccf620fc76c
Lozovoi-Rodion <rodionlozovoi@gmail.com>
2021-02-26 19:03:02 +0200

    git ignore added:

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,6 +1,17 @@
 import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
+  static getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const page = ctx.renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html>
